0f45bc57347b9156f51185c8c61f05ec2ed8d000
lenovo <dgon1976@mail.ru>
2022-10-16 19:31:12 +0500

    4.6 выполнено - правки по разбору ДЗ

--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -3,6 +3,7 @@ package ru.hogwarts.school.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.hogwarts.school.model.Avatar;
@@ -20,7 +21,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
@@ -30,7 +30,7 @@ public class StudentServiceImpl implements StudentService {
     private final StudentRepository studentRepository;
     private final AvatarRepository avatarRepository;
 
-    Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     @Value("${avatars.dir.path}")
     private String avatarsDir;
@@ -41,37 +41,37 @@ public class StudentServiceImpl implements StudentService {
     }
 
     public Student addStudent(Student student) {
-        logger.info("Was invoked method for create student with body {}", student);
+        LOG.info("Was invoked method for create student with body {}", student);
         return studentRepository.save(student);
     }
 
     public Student findStudent(Long id) {
-        logger.info("Was invoked method for getting  student by id: {}", id);
+        LOG.info("Was invoked method for getting  student by id: {}", id);
         return studentRepository.findById(id).orElse(null);
     }
 
     public Student editStudent(Student student) {
-        logger.info("Was invoked method for update student with body {}", student);
+        LOG.info("Was invoked method for update student with body {}", student);
         return studentRepository.save(student);
     }
 
     public void deleteStudent(Long id) {
-        logger.info("Was invoked method for delete student by id: {}", id);
+        LOG.info("Was invoked method for delete student by id: {}", id);
         studentRepository.deleteById(id);
     }
 
     public Collection<Student> findByAge(int age) {
-        logger.info("Was invoked method for find collection of students by age: {}", age);
+        LOG.info("Was invoked method for find collection of students by age: {}", age);
         return studentRepository.findAllByAge(age);
     }
 
     public Avatar findAvatar(long studentId) {
-        logger.info("Was invoked method for find Avatar");
+        LOG.info("Was invoked method for find Avatar");
         return avatarRepository.findByStudentId(studentId).orElse(null);
     }
 
     public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
-        logger.info("Was invoked method for upload Avatar");
+        LOG.info("Was invoked method for upload Avatar");
         Student student = findStudent(studentId);
         Path filePath = Path.of(avatarsDir, student + "." + getExtensions(Objects.requireNonNull(file.getOriginalFilename())));
         Files.createDirectories(filePath.getParent());
@@ -98,35 +98,35 @@ public class StudentServiceImpl implements StudentService {
     }
 
     public Collection<Student> getAll() {
-        logger.info("Was invoked method for getting  collection of all students");
+        LOG.info("Was invoked method for getting  collection of all students");
         return studentRepository.findAll();
     }
 
     @Override
     public Integer getCountOfAllStudents() {
-        logger.info("Was invoked method for get count of all students");
+        LOG.info("Was invoked method for get count of all students");
         return studentRepository.getCountOfAllStudents();
     }
 
     @Override
     public Double getAverageAgeOfStudents() {
-        logger.info("Was invoked method for get Average Age Of Students");
+        LOG.info("Was invoked method for get Average Age Of Students");
         return studentRepository.getAverageAgeOfStudents();
     }
 
     @Override
     public Collection<Student> getLastFiveStudents() {
-        logger.info("Was invoked method for get Last Five Students");
+        LOG.info("Was invoked method for get Last Five Students");
         return studentRepository.getLastFiveStudents();
     }
 
     public Set<Student> findByAgeBetween(int minAge, int maxAge) {
-        logger.info("Was invoked method for find collection of student with age ({} - {})", minAge, maxAge);
+        LOG.info("Was invoked method for find collection of student with age ({} - {})", minAge, maxAge);
         return (Set<Student>) studentRepository.findByAgeBetween(minAge, maxAge);
     }
 
     public Faculty getFacultyOfStudent(Long id) {
-        logger.info("Was invoked method for getting faculty by studentId: {}", id);
+        LOG.info("Was invoked method for getting faculty by studentId: {}", id);
         Student student = studentRepository.findById(id).orElse(null);
         if (student != null) {
             return student.getFaculty();
@@ -153,60 +153,94 @@ public class StudentServiceImpl implements StudentService {
                 .orElse(0);
     }
 
-    @Override
+//    @Override
+//    public void getNameStudentsThread() {
+//        Stream<Student> streamStudent = studentRepository.findAll().stream();
+//        List<String> studentsName = streamStudent
+//                .map(a -> a.getName())
+//                .collect(Collectors.toList());
+//
+//        System.out.println(studentsName);
+//        System.out.println("////////////////////////////////");
+//
+//        System.out.println(studentsName.get(0));
+//        System.out.println(studentsName.get(1));
+//
+//        new Thread(() ->
+//        {
+//            System.out.println(studentsName.get(2));
+//            System.out.println(studentsName.get(3));
+//        }).start();
+//
+//        new Thread(() ->
+//        {
+//            System.out.println(studentsName.get(4));
+//            System.out.println(studentsName.get(5));
+//        }).start();
+//
+//    }
+
+//    @Override
+//    public void getNameStudentsThreadSynch() {
+//        Stream<Student> streamStudent = studentRepository.findAll().stream();
+//        List<String> studentsName = streamStudent
+//                .map(Student::getName)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(studentsName);
+//        System.out.println("////////////////////////////////");
+//
+//        synchronized (studentsName) {
+//
+//            printStudent(studentsName.get(0));
+//            printStudent(studentsName.get(1));
+//
+//            new Thread(() ->
+//            {
+//                printStudent(studentsName.get(2));
+//                printStudent(studentsName.get(3));
+//            }).start();
+//
+//            new Thread(() ->
+//            {
+//                printStudent(studentsName.get(4));
+//                printStudent(studentsName.get(5));
+//            }).start();
+//        }
+//    }
+//
+//    private void printStudent(String name) {
+//        System.out.println(name);
+//    }
+
     public void getNameStudentsThread() {
-        Stream<Student> streamStudent = studentRepository.findAll().stream();
-        List<String> studentsName = streamStudent
-                .map(a -> a.getName())
-                .collect(Collectors.toList());
+        List<Student> students = studentRepository.findAll(PageRequest.of(0, 6)).getContent();
 
-        System.out.println(studentsName);
-        System.out.println("////////////////////////////////");
-
-        System.out.println(studentsName.get(0));
-        System.out.println(studentsName.get(1));
-
-        new Thread(() ->
-        {
-            System.out.println(studentsName.get(2));
-            System.out.println(studentsName.get(3));
-        }).start();
-
-        new Thread(() ->
-        {
-            System.out.println(studentsName.get(4));
-            System.out.println(studentsName.get(5));
-        }).start();
-
+        getNameStudentsThread(students.subList(0, 2));
+        new Thread(() -> getNameStudentsThread(students.subList(2, 4))).start();
+        new Thread(() -> getNameStudentsThread(students.subList(4, 6))).start();
     }
 
-    @Override
+    private void getNameStudentsThread(List<Student> students) {
+        for (Student student : students) {
+            LOG.info(student.getName());
+        }
+    }
+
+    private synchronized void getNameStudentsThreadSynch(List<Student> students) {
+        for (Student student : students) {
+            LOG.info(student.getName());
+        }
+    }
+
     public void getNameStudentsThreadSynch() {
-        Stream<Student> streamStudent = studentRepository.findAll().stream();
-        List<String> studentsName = streamStudent
-                .map(Student::getName)
-                .collect(Collectors.toList());
+        List<Student> students = studentRepository.findAll(PageRequest.of(0, 6)).getContent();
 
-        System.out.println(studentsName);
-        System.out.println("////////////////////////////////");
+        getNameStudentsThreadSynch(students.subList(0, 2));
+        new Thread(() -> getNameStudentsThreadSynch(students.subList(2, 4))).start();
+        new Thread(() -> getNameStudentsThreadSynch(students.subList(4, 6))).start();
 
-        printStudent(studentsName.get(0));
-        printStudent(studentsName.get(1));
-
-        new Thread(() ->
-        {
-            printStudent(studentsName.get(2));
-            printStudent(studentsName.get(3));
-        }).start();
-
-        new Thread(() ->
-        {
-            printStudent(studentsName.get(4));
-            printStudent(studentsName.get(5));
-        }).start();
     }
 
-    private synchronized void printStudent(String name) {
-        System.out.println(name);
-    }
+
 }
